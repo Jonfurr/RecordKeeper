13f3bc60e25a6fcdc78e76a03f3a079e1abd33bf
Luke Duncan <luke.will.duncan@gmail.com>
2015-09-30 15:44:05 -0500

    tracks users logins, last activity, last logout, last ip address logged in at

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150929180727) do
+ActiveRecord::Schema.define(version: 20150930203820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,16 +37,21 @@ ActiveRecord::Schema.define(version: 20150929180727) do
   add_index "albums", ["user_id"], name: "index_albums_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",            null: false
+    t.string   "email",                      null: false
     t.string   "pic_url"
-    t.string   "name",             null: false
+    t.string   "name",                       null: false
     t.string   "favorite"
     t.string   "crypted_password"
     t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "last_login_at"
+    t.datetime "last_logout_at"
+    t.datetime "last_activity_at"
+    t.string   "last_login_from_ip_address"
   end
 
+  add_index "users", ["last_logout_at", "last_activity_at"], name: "index_users_on_last_logout_at_and_last_activity_at", using: :btree
   add_index "users", ["name", "email"], name: "index_users_on_name_and_email", unique: true, using: :btree
 
   add_foreign_key "albums", "users"
