1cd194b287297223699081f344a7fd403d4d19d4
Natalie Martell <nrmartell@gmail.com>
2015-09-30 15:46:19 -0500

    user profile page functional

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
    @user = User.find(params[:id])
+   @albums= Album.where(user_id: params[:id])
   end
 
   # GET /users/new
@@ -34,6 +35,7 @@ class UsersController < ApplicationController
       if @user.save
           format.html { redirect_to(:users, :notice => 'Registration successfull.') }
           format.json { render :show, status: :created, location: @user }
+          
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
