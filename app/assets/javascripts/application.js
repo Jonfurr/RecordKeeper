f7fed0a2d283b23518c4fdad318111129182b5d1
Adam Weitzman <adamcweitzman@gmail.com>
2015-10-02 09:43:22 -0500

    firday start

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,22 @@
 // ...
 //= require jquery
 //= require_tree
+// console.log("test")
+// $('#album_tracklist').on('click', function(){
+// 	alert("hi")
+// });
+
+// $('document').on('click', function(){
+// 	alert("hi")
+// })
+
+
+var valueHolder = [];
+
+$('#button-tracklist').on('click', function(){
+
+ valueHolder.push($('#album_tracklist').val());
+
+ $('#album_tracklist').val("");
+
+}) 
--- app/views/albums/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @album, :id, :artist, :year, :title, :pressing, :label, :genre, :image, :tracklist, :country, :comment, :favorite, :created_at, :updated_at
